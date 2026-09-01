ae8baa3f4461fd7112b298e7b586a60d8c763063
Danielle Soares <contato.daniellesoares@gmail.com>
2026-09-01 11:53:08 -0300

    ✨ New Feature:  Configuração das rotas da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Navbar from './components/navbar/Navbar'
 import Home from './pages/home/Home'
 import Footer from './components/footer/Footer'
 
 function App() {
-
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
       <Footer />
-    </>
+    </BrowserRouter>
   )
 }
 
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,15 +1,19 @@
+import { Link } from 'react-router-dom'
+
 function Navbar() {
     return (
         <nav className="bg-emerald-700 text-white">
             <div className="container mx-auto flex items-center justify-between py-4 px-6">
-                <div>
-                    <h1 className="text-2xl font-bold">
-                        FarmaVida
-                    </h1>
-                </div>
+                <Link to="/home">
+                <h1 className="text-2xl font-bold">
+                    FarmaVida
+                </h1>
+                </Link>
 
                 <div className="flex items-center gap-6">
-                    <span>Home</span>
+                    <Link to="/home">
+                    Home
+                    </Link>
                     <span>Categorias</span>
                     <span>Cadastrar Categoria</span>
                 </div>
